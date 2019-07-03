92ad73efccd94829bb9731d03bec47e018cb6c7a
Tajir Hasnain <31435079+Tajir-Hasnain@users.noreply.github.com>
2019-07-03 23:53:58 +0600

    Create Make Toast.java

---
+++ Toast/Make Toast.java
@@ -0,0 +1,6 @@
+/*
+* Change LENGTH_SHORT to desired length for changing toast time
+*/
+private void MakeToast(String message) {
+        Toast.makeText(getApplicationContext() , message, Toast.LENGTH_SHORT).show();
+    }
